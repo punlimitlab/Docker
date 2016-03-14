98056878268f958a8735e0f57865114b30267b3b
Pierre-Henri Gache <Pierre-Henri Gache>
2016-03-14 22:38:02 +0100

    Add push command

--- BaseImages/build_base_arm.sh
+++ BaseImages/build_base_arm.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-if docker images | grep aspnetbase;
+if docker images | grep punlimitlab/aspnetbase | grep latest;
 then
 	echo "Image already exist"
 else
 	echo "Image does not exist, creating it"
 	mkdir /tmp/dockertmp
 	cp Dockerfile.base.arm /tmp/dockertmp/Dockerfile
-	docker build -t aspnetbase /tmp/dockertmp
+	docker build -t punlimitlab/aspnetbase:latest /tmp/dockertmp
 	rm -Rf /tmp/dockertmp
+	
+	echo "Push on docker hub"
+	docker push punlimitlab/aspnetbase:latest
 fi
 
 echo "Done!"
--- BaseImages/build_base_arm_alpha.sh
+++ BaseImages/build_base_arm_alpha.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-if docker images | grep aspnetbase_alpha;
+if docker images | grep punlimitlab/aspnetbase | grep alpha;
 then
 	echo "Image already exist"
 else
 	echo "Image does not exist, creating it"
 	mkdir /tmp/dockertmp
 	cp Dockerfile.base.arm_alpha /tmp/dockertmp/Dockerfile
-	docker build -t aspnetbase_alpha /tmp/dockertmp
+	docker build -t punlimitlab/aspnetbase:alpha /tmp/dockertmp
 	rm -Rf /tmp/dockertmp
+	
+	echo "Push on docker hub"
+	docker push punlimitlab/aspnetbase:alpha
 fi
 
 echo "Done!"
